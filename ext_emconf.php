5a1c2a8134d829532a2a179767206b36af9e0e90
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2014-06-14 22:26:13 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cc_setsearch".
+ *
+ * Auto generated 14-06-2014 19:58
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Web>Func, Wizards, Set pages searchable',
+	'description' => 'Set/unset the searchable flag for pages recursively.',
+	'category' => 'module',
+	'shy' => '0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => '0',
+	'lockType' => '',
+	'author' => 'René Fritz',
+	'author_email' => 'r.fritz@colorcube.de',
+	'author_company' => 'Colorcube - digital media lab',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'version' => '1.2.0',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.0.0-6.2.99',
+			'func_wizards' => '6.0.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
